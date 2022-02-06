d84f5c4aa544dea1435732ca0c8256d87de1eefc
Nicole Neo <nicolen@gmail.com>
2022-02-06 13:07:47 +0000

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+## Data
+Scraped categories from https://www.counselling-directory.org.uk/adv-search.html
+
+```javascript
+const categories = [];
+$("#search-filter-services option").each(function () {const that = $(this); categories.push(that.text());});
+```
+
+Most therapy websites use AJAX so I couldn't scrape from their API directly.
+
+
+## Assumptions
+Therapists works 9-5 to simplify slots. (Obviously it's better to have out-of-hours but it's more complex to seed)
+Slots are bookable hourly.
+Assume all 9-5 weekday slots are available. No holidays.
+
+Seeder has therapists
+The booking seeder books 20% of the available slots in the next 3 months
+
+## Steps
+Initially seed therapists and specialities
+run book slots script a few times to randomly book slots
+ability to book remaining slots
---
+++ appointment-slot.js
@@ -0,0 +1,48 @@
+"use strict";
+const faker = require("@faker-js/faker");
+const moment = require("moment");
+const fs = require("fs");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
+ * to customize this service
+ */
+
+const specialities = () => {
+  const specialities = [];
+  const specialitiesFile = __dirname + "/therapy_categories.txt";
+  fs.readFile(specialitiesFile, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const lines = data.split("\n");
+    let parentCategory = "";
+    for (let line in lines) {
+      const speciality = line.trim();
+      let isChild = false;
+      if (speciality.startsWith("-")) {
+        parentCategory = speciality;
+        isChild = true;
+      }
+      const specialityObj = {
+        name: speciality,
+      };
+      if (isChild) {
+        specialityObj.parent = parentCategory;
+      }
+      specialities.push(specialityObj);
+    }
+  });
+  return specialities;
+};
+
+module.exports = {
+  generateDates: async (qIds) => {
+    console.log("generate dates");
+    const today = moment();
+    const threeMonths = today.add("3", "months");
+    const futureDates = faker.date.betweens(today, threeMonths, 100);
+    console.log(futureDates);
+  },
+};
